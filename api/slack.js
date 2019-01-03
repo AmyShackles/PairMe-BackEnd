bafeec06eed237adddcb996b149dd3280281e116
Amy Shackles <amyshackles@gmail.com>
2019-01-02 18:09:54 -0800

    Barebones code for Slack integration

---
+++ api/slack.js
@@ -0,0 +1,18 @@
+const Slackbot = require("slackbots")
+
+const bot = new Slackbot({
+  token: process.env.SLACK_TOKEN,
+  name: "pair_bot"
+})
+bot.on("start", function() {
+  var params = {
+    icon_emoji: ":cat:"
+  }
+})
+
+bot.on("error", err => console.log(err))
+
+bot.postMessageChannel(
+  "pair_me",
+  `Hey, ${user1}, ${user2} is available to help!`
+)
