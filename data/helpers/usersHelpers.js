6efd82f853ee95b5a496d936116405303e18b065
NandoTheessen <nando.theessen@gmail.com>
2019-01-03 02:47:03 +0100

    refactor(): refactor userRouter to allow for input validation seperate from register

--- data/helpers/usersHelpers.js
+++ data/helpers/usersHelpers.js
@@ -1,80 +1,81 @@
-const db = require('../dbConfig.js');
+const db = require("../dbConfig.js")
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs")
 
 module.exports = {
-    getUsers,
-    registerUser,
-    availableEmail,
-    availableUsername,
-    availableHandle
+  getUsers,
+  registerUser,
+  availableEmail,
+  availableUsername,
+  availableHandle
 }
 
 // return all users, empty array if no users
 function getUsers() {
-    return db('users')
-        .select('id', 'username', 'email', 'slack_handle');
-};
+  return db("users").select("id", "username", "email", "slack_handle")
+}
 
 // register new user if valid
-function registerUser(user) {
-    const newUser = user;
-    const hash = bcrypt.hashSync(newUser.password, 14);
-    newUser.username = newUser.username.toLowerCase();
-    newUser.email = newUser.email.toLowerCase();
-    newUser.slack_handle = newUser.slack_handle.toLowerCase();
-    newUser.password = hash;
+async function registerUser(user) {
+  const newUser = user
+  const hash = await bcrypt
+    .hash(newUser.password, 14)
+    .catch(e => console.log("hashing function error!"))
+  newUser.username = newUser.username.toLowerCase()
+  newUser.email = newUser.email.toLowerCase()
+  newUser.slack_handle = newUser.slack_handle.toLowerCase()
+  newUser.password = hash
 
-    return db('users')
-        .insert(newUser)
-        .then(id => {
-            console.log(id);
-            return id;
-        })
-        .catch(err => {
-            return false;
-        });
-
-};
+  console.log(newUser)
+  return db("users")
+    .insert(newUser)
+    .then(id => {
+      console.log(id)
+      return id
+    })
+    .catch(err => {
+      return false
+    })
+}
 
 // true if username is not in database
 function availableUsername(name) {
-    name = name.toLowerCase();
-    return db('users')
-        .where({ username: name })
-        .then(result => {
-            if(result.length){
-                return false;
-            } else {
-                return true;
-            }
-        });
-};
+  name = name.toLowerCase()
+  return db("users")
+    .where({ username: name })
+    .then(result => {
+      if (result.length) {
+        return false
+      } else {
+        return true
+      }
+    })
+}
 
 // true if email not in database
 function availableEmail(name) {
-    name = name.toLowerCase();
-    return db('users')
-        .where({ email: name })
-        .then(result => {
-            if(result.length){
-                return false;
-            } else {
-                return true;
-            }
-        });
-};
+  name = name.toLowerCase()
+  return db("users")
+    .where({ email: name })
+    .then(result => {
+      if (result.length) {
+        return false
+      } else {
+        return true
+      }
+    })
+}
 
 // true if email not in database
 function availableHandle(name) {
-    name = name.toLowerCase();
-    return db('users')
-        .where({ slack_handle: name })
-        .then(result => {
-            if(result.length){
-                return false;
-            } else {
-                return true;
-            }
-        });
-};
+  name = name.toLowerCase()
+  return db("users")
+    .where({ slack_handle: name })
+    .then(result => {
+      if (result.length) {
+        return false
+      } else {
+        return true
+      }
+    })
+}
