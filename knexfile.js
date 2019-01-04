c2b8f12b4211282c4517c64353693fedfd54d6c8
NandoTheessen <nando.theessen@gmail.com>
2019-01-04 06:36:00 +0100

    Add pg client & db connection

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
@@ -16,14 +15,14 @@ module.exports = {
     },
     seeds: {
       directory: './data/seeds'
-    },
+    }
   },
 
   staging: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -36,12 +35,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
@@ -50,5 +45,4 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
-
-};
+}
