906ca75d289d241d181464084831725bf63c163c
NandoTheessen <nando.theessen@gmail.com>
2019-01-04 06:23:36 +0100

    Changed userRouter to register new users

--- data/helpers/usersHelpers.js
+++ data/helpers/usersHelpers.js
@@ -1,6 +1,6 @@
-const db = require("../dbConfig.js")
+const db = require('../dbConfig.js')
 
-const bcrypt = require("bcryptjs")
+const bcrypt = require('bcryptjs')
 
 module.exports = {
   getUsers,
@@ -12,28 +12,30 @@ module.exports = {
 
 // return all users, empty array if no users
 function getUsers() {
-  return db("users").select("id", "username", "email", "slack_handle")
+  return db('users').select('id', 'username', 'email', 'slack_handle')
 }
 
 // register new user if valid
-async function registerUser(user) {
-  const hash = await bcrypt
-    .hash(user.password, 12)
-    .catch(e => console.log("hashing function error!"))
-  user.username = user.username.toLowerCase()
-  user.email = user.email.toLowerCase()
-  user.slack_handle = user.slack_handle.toLowerCase()
-  user.password = hash
+async function registerUser(data) {
+  const { access_token } = data
+  const { id, name, email, avatar_512 } = data.user
+  const user = {
+    id,
+    username: name,
+    email,
+    avatar: avatar_512,
+    access_token
+  }
 
   // no catch to let the error bubble up for the "parent" try / catch block
-  const id = await db("users").insert(user)
-  return id
+  const u_id = await db('users').insert(user)
+  return u_id
 }
 
 // true if username is not in database
 function availableUsername(name) {
   name = name.toLowerCase()
-  return db("users")
+  return db('users')
     .where({ username: name })
     .then(result => {
       if (result.length) {
@@ -47,7 +49,7 @@ function availableUsername(name) {
 // true if email not in database
 function availableEmail(name) {
   name = name.toLowerCase()
-  return db("users")
+  return db('users')
     .where({ email: name })
     .then(result => {
       if (result.length) {
@@ -61,7 +63,7 @@ function availableEmail(name) {
 // true if email not in database
 function availableHandle(name) {
   name = name.toLowerCase()
-  return db("users")
+  return db('users')
     .where({ slack_handle: name })
     .then(result => {
       if (result.length) {
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -51,18 +51,7 @@ router.post('/slackhandle', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-  //'https://slack.com/api/oauth.access?client_id=154966377728.516246535895
-  //&client_secret=8038c1564ae0eadcd2264240f41844a
-  //&code=154966377728.515573136256.2ad81449ee60f61a8e08161e7ecb8def8fe98400f4519d123af8408bdc6cf814'
   const { token } = req.body
-  console.log(
-    'clientid',
-    process.env.CLIENTID,
-    'clientsecret',
-    process.env.CLIENTSECRET,
-    'token',
-    token
-  )
   const response = await axios
     .get(
       `https://slack.com/api/oauth.access?client_id=${
@@ -71,12 +60,11 @@ router.post('/login', async (req, res) => {
     )
     .catch(e => console.log('OAuth failure!', e))
 
+  const avail = await usersDb.availableUsername(response.data.user.name)
+  if (avail) {
+    const id = await usersDb.registerUser(response.data)
+  }
   res.status(200).send({ data: response.data })
-
-  // const userData = await axios
-  //   .get(`https://slack.com/api/users.identity?token=${access_token}&pretty=1`)
-  //   .catch(e => console.log('Error getting user Data', e))
-  // console.log(userData)
 })
 
 router.post('/register', async (req, res) => {
