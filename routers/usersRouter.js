33c3a241830bbae2ebcd3cd369c51c4b01a9f609
NandoTheessen <nando.theessen@gmail.com>
2019-01-04 02:57:11 +0100

    Add login route to handle auth w/ slack

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,10 +1,11 @@
-require("dotenv").config()
+require('dotenv').config()
 
-const express = require("express")
+const express = require('express')
+const axios = require('axios')
 
-const jwt = require("jsonwebtoken")
+const jwt = require('jsonwebtoken')
 
-const usersDb = require("../data/helpers/usersHelpers.js")
+const usersDb = require('../data/helpers/usersHelpers.js')
 
 const router = express.Router()
 
@@ -17,10 +18,10 @@ const generateToken = user => {
     slackHandle: user.slack_handle
   }
 
-  const secret = process.env.JWT_SECRET || "super secret!" // don't forget to update production environment
+  const secret = process.env.JWT_SECRET || 'super secret!' // don't forget to update production environment
 
   const options = {
-    expiresIn: "6h"
+    expiresIn: '6h'
   }
 
   return jwt.sign(payload, secret, options)
@@ -41,28 +42,41 @@ const generateToken = user => {
 
 // [POST] /api/users/register
 // registers an new user
-router.post("/checkuser", async (req, res) => {
+router.post('/checkuser', async (req, res) => {
   const { username } = req.body
   const availableUsername = await usersDb.availableUsername(username)
   if (availableUsername) return res.status(200).end()
-  return res.status(400).send({ err: "Name taken!" })
+  return res.status(400).send({ err: 'Name taken!' })
 })
 
-router.post("/checkmail", async (req, res) => {
+router.post('/checkmail', async (req, res) => {
   const { email } = req.body
   const availableEmail = await usersDb.availableEmail(email)
   if (availableEmail) return res.status(200).end()
-  return res.status(400).send({ err: "email taken!" })
+  return res.status(400).send({ err: 'email taken!' })
 })
 
-router.post("/slackhandle", async (req, res) => {
+router.post('/slackhandle', async (req, res) => {
   const { slackhandle } = req.body
   const availableHandle = await usersDb.availableHandle(slackhandle)
   if (availableHandle) return res.status(200).end()
-  return res.status(400).send({ err: "Handle taken!" })
+  return res.status(400).send({ err: 'Handle taken!' })
 })
 
-router.post("/register", async (req, res) => {
+router.post('/login', async (req, res) => {
+  const { token } = req.params
+  const { access_token } = await axios
+    .get(
+      `https://slack.com/api/oauth.access?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}${token}`
+    )
+    .catch(e => console.log('OAuth failure!', e))
+  const userData = await axios
+    .get(`https://slack.com/api/users.identity?token=${access_token}&pretty=1`)
+    .catch(e => console.log('Error getting user Data', e))
+  res.status(200).send({ userData })
+})
+
+router.post('/register', async (req, res) => {
   const newUser = req.body
   try {
     const insertResponse = await usersDb.registerUser(newUser)
@@ -71,7 +85,7 @@ router.post("/register", async (req, res) => {
 
     res.status(201).json({ insertResponse, token })
   } catch (err) {
-    res.status(500).json({ message: "Error registering new user", err })
+    res.status(500).json({ message: 'Error registering new user', err })
   }
 })
 
