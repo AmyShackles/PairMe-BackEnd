bb0c13f27b7d598ca975769411bea1ea56e9569f
NandoTheessen <nando.theessen@gmail.com>
2019-01-04 16:18:52 +0100

    remove unecessary routes

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -10,47 +10,6 @@ const usersDb = require('../data/helpers/usersHelpers.js')
 
 const router = express.Router()
 
-// generates jwt
-const generateToken = user => {
-  const payload = {
-    subject: user.id,
-    username: user.username,
-    email: user.email,
-    slackHandle: user.slack_handle
-  }
-
-  const secret = process.env.JWT_SECRET || 'super secret!' // don't forget to update production environment
-
-  const options = {
-    expiresIn: '6h'
-  }
-
-  return jwt.sign(payload, secret, options)
-}
-
-// [POST] /api/users/register
-// registers an new user
-router.post('/checkuser', async (req, res) => {
-  const { username } = req.body
-  const availableUsername = await usersDb.availableUsername(username)
-  if (availableUsername) return res.status(200).end()
-  return res.status(400).send({ err: 'Name taken!' })
-})
-
-router.post('/checkmail', async (req, res) => {
-  const { email } = req.body
-  const availableEmail = await usersDb.availableEmail(email)
-  if (availableEmail) return res.status(200).end()
-  return res.status(400).send({ err: 'email taken!' })
-})
-
-router.post('/slackhandle', async (req, res) => {
-  const { slackhandle } = req.body
-  const availableHandle = await usersDb.availableHandle(slackhandle)
-  if (availableHandle) return res.status(200).end()
-  return res.status(400).send({ err: 'Handle taken!' })
-})
-
 router.post('/login', async (req, res) => {
   const { token } = req.body
   const response = await axios
