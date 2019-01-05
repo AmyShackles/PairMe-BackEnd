85fdf438d95071ac06f28a12a082d256975d55ed
Amy Shackles <amyshackles@gmail.com>
2019-01-05 12:23:57 -0800

    Updated webhook to newest

--- routers/slackRouter.js
+++ routers/slackRouter.js
@@ -60,7 +60,7 @@ router.post('/', async (req, res) => {
       }
       const message = `Hey, <@${user1}>, <@${user2}> is available to help!`
       axios.post(
-        'https://hooks.slack.com/services/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',
+        'https://hooks.slack.com/services/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',
         { text: message }
       )
     }
@@ -75,14 +75,14 @@ router.post('/', async (req, res) => {
       }
       const message = `Hey, <@${user1}>, <@${user2}> is available to help!`
       axios.post(
-        'https://hooks.slack.com/services/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',
+        'https://hooks.slack.com/services/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',
         { text: message }
       )
     }
   } else {
     const message = `Hey, <@${user}>, I do not understand that command.`
     axios.post(
-      'https://hooks.slack.com/services/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',
+      'https://hooks.slack.com/services/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',
       { text: message }
     )
   }
